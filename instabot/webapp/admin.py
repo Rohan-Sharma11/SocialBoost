bbccf7e1cbe45ede33a0b758588f68bbe1dc2e83
Rohan Sharma <rohansh3535@gmail.com>
2024-09-21 00:52:20 +0530

    added webapp in the settings.py, created two models and registered them in admin.py

--- instabot/webapp/admin.py
+++ instabot/webapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import InstaUser, BotRequest
 
 # Register your models here.
+
+admin.site.register([
+    InstaUser,
+    BotRequest
+])
--- instabot/webapp/models.py
+++ instabot/webapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class InstaUser(models.Model):
+    email_id = models.EmailField()
+
+
+
+
+class BotRequest(models.Model):
+    request_type = models.CharField(max_length=10)
+
